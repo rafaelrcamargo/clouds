767957b26fb003b685744339dfb3f0107155f529
Rafael R. Camargo <rafaelrakochinski@gmail.com>
2024-03-15 19:10:17 -0300

    feat: initial structure

---
+++ src/components/cloud.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import * as THREE from "three"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { Clouds, Cloud, OrbitControls } from "@react-three/drei"
+import { motion as m, useScroll, useTransform } from "framer-motion"
+import { FC, PropsWithChildren } from "react"
+
+const Wrapper: FC<PropsWithChildren> = ({ children }) => {
+  const { scrollYProgress } = useScroll()
+  const opacitySpan = useTransform(scrollYProgress, [0, 0.25], [1, 0])
+  const opacityH1 = useTransform(scrollYProgress, [0, 0.5], [1, 0])
+  const letterSpacing = useTransform(scrollYProgress, [0, 0.5], ["1px", "16px"])
+  const letterSpacing2 = useTransform(
+    scrollYProgress,
+    [0, 0.35, 0.5],
+    ["1px", "1px", "104px"]
+  )
+
+  return (
+    <>
+      <m.span
+        style={{ opacity: opacitySpan, letterSpacing }}
+        className="absolute left-1/2 top-1/2 z-20 flex -translate-x-1/2 -translate-y-1/2 flex-wrap whitespace-pre text-4xl font-black text-neutral-200 drop-shadow-[0_0_8px_#0006] xl:-z-20 xl:flex-nowrap xl:gap-6 xl:text-8xl">
+        The<span className="italic">future-proof</span>cloud.
+      </m.span>
+
+      <m.h1
+        style={{ opacity: opacityH1, letterSpacing: letterSpacing2 }}
+        className="absolute left-1/2 top-1/2 z-20 flex -translate-x-1/2 -translate-y-1/2 flex-wrap whitespace-pre text-4xl font-black text-transparent xl:flex-nowrap xl:gap-6 xl:text-8xl">
+        The
+        <span className="z-20 italic text-neutral-200 drop-shadow-[0_0_8px_#0003]">
+          future-proof
+        </span>
+        cloud.
+      </m.h1>
+
+      {children}
+    </>
+  )
+}
+
+export const Cloudd = () => (
+  <Wrapper>
+    <Canvas camera={{ position: [0, 60, 0] }}>
+      <Inner />
+      <OrbitControls autoRotate autoRotateSpeed={0.5} enableZoom={false} />
+    </Canvas>
+  </Wrapper>
+)
+
+const Inner = () => {
+  useFrame(({ camera }) => {
+    camera.position.y = -window.scrollY * 0.03 + 60
+  })
+
+  return (
+    <Clouds material={THREE.MeshBasicMaterial}>
+      <Cloud
+        volume={24}
+        segments={56}
+        bounds={[8, 16, 2]}
+        position={[0, 25, 0]}
+        color="#e0e0e0"
+        concentrate="inside"
+      />
+    </Clouds>
+  )
+}
