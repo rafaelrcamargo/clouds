bc4647ad0a31ce8c92034eaa3c8c2dcd4af0c9fe
Rafael R. Camargo <rafaelrakochinski@gmail.com>
2024-03-15 23:22:47 -0300

    feat: travel faster throught the cloud

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={cn(GeistSans.variable, "bg-neutral-950")}>
         {children}
-        <div className="overlay" />
+        <div className="overlay z-50" />
       </body>
     </html>
   )
